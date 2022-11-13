baabf403dfdc1f130bbccbcd4dae3cbeef76cae5
neeleshishu <112829458+neeleshishu@users.noreply.github.com>
2022-11-13 16:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-alert('if you are using mobile use dekstop mode for better experience');
+
 const info = async(event)=>{
 const api = await fetch(`
 https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadbf693e&offset=0`);
         const dat = await api.json();
-        console.log(dat);
-        document.getElementById('type').innerText = dat.data[0].matchType;
+        try{
+        if(!dat.data[0].score[1])
+        {
+            document.getElementById('type').innerText = dat.data[0].matchType;
         document.getElementById('name').innerText = dat.data[0].name;
         document.getElementById('inning').innerText = dat.data[0].score[0].inning;
         document.getElementById('run').innerText = dat.data[0].score[0].r;
@@ -13,6 +15,29 @@ https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadb
         document.getElementById('date').innerText =dat.data[0].date;
         document.getElementById('opp1').innerText =dat.data[0].teamInfo[0].name;
         document.getElementById('opp2').innerText =dat.data[0].teamInfo[1].name;
+        document.getElementById('status').innerText = dat.data[0].status;
+        
+    }else{
+        document.getElementById('type').innerText = dat.data[0].matchType;
+        document.getElementById('name').innerText = dat.data[0].name;
+        document.getElementById('inning').innerText = dat.data[0].score[1].inning;
+        document.getElementById('run').innerText = dat.data[0].score[1].r;
+        document.getElementById('wicket').innerText =dat.data[0].score[1].w;
+        document.getElementById('over').innerText =dat.data[0].score[1].o;
+        document.getElementById('date').innerText =dat.data[0].date;
+        document.getElementById('opp1').innerText =dat.data[0].teamInfo[0].name;
+        document.getElementById('opp2').innerText =dat.data[0].teamInfo[1].name;
+        document.getElementById('status').innerText = dat.data[0].status;
+    }
+}catch{
+        document.getElementById('status').innerText = dat.status;
+        document.getElementById('name').innerText = dat.reason;
+        document.getElementById('inning').innerText = 'api hits there max refresh for today';
+
+
+    
+}
+        
 
     }
 
