9f5fd3409ef0d9bc67036e30b4aaf387957a2ea5
neeleshishu <112829458+neeleshishu@users.noreply.github.com>
2022-11-15 19:16:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 alert('if you are using this in mobile then use desktop mode for better experience');
+//alert('if you are using this in mobile then use desktop mode for better experience');
 const info = async (event) => {
+    try{
     const api = await fetch(`
-https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadbf693e&offset=0`).then( (dat) =>{
-    dat = api.json()
-}).catch( (error) => {
-    console.log(error);
-});
+https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadbf693e&offset=0`);
+const dat = await api.json();
+console.log(dat);
     
-    try {
-        if (!dat.data[0]) {
-            document.getElementById('status').innerText = dat.status;
-            document.getElementById('name').innerText = dat.reason;
-            document.getElementById('inning').innerText = 'api hits there max refresh for today';
-        } else if (!dat.data[0].score[1]) {
+    
+       if (!dat.data[0].score[1]) {
             document.getElementById('type').innerText = dat.data[0].matchType;
             document.getElementById('name').innerText = dat.data[0].name;
             document.getElementById('inning').innerText = dat.data[0].score[0].inning;
@@ -23,8 +19,8 @@ https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadb
             document.getElementById('opp1').innerText = dat.data[0].teamInfo[0].name;
             document.getElementById('opp2').innerText = dat.data[0].teamInfo[1].name;
             document.getElementById('status').innerText = dat.data[0].status;
-            document.getElementById('img1').innerHTML = `<img src="${dat.data[0].teamInfo[1].name}">`;
-            document.getElementById('img2').innerHTML = `<img src="${dat.data[0].teamInfo[1].name}">`;
+            document.getElementById('img1').innerHTML = `<img src="${dat.data[0].teamInfo[0].img}">`;
+            document.getElementById('img2').innerHTML = `<img src="${dat.data[0].teamInfo[1].img}">`;
 
         } else {
             document.getElementById('type').innerText = dat.data[0].matchType;
@@ -37,10 +33,13 @@ https://api.cricapi.com/v1/currentMatches?apikey=40d0cb22-ed60-49c9-b417-4fdbadb
             document.getElementById('opp1').innerText = dat.data[0].teamInfo[0].name;
             document.getElementById('opp2').innerText = dat.data[0].teamInfo[1].name;
             document.getElementById('status').innerText = dat.data[0].status;
-        }
-    } catch {
+            document.getElementById('img1').innerHTML = `<img src="${dat.data[0].teamInfo[0].img}">`;
+            document.getElementById('img2').innerHTML = `<img src="${dat.data[0].teamInfo[1].img}">`;
 
-        document.getElementById('inning').innerText = 'api hits there max refresh for today';
+        } 
+     } catch(e) {
+        console.log(e);
+        document.getElementById('inning').innerText = 'Some Error Occured with an API';
 
 
 
